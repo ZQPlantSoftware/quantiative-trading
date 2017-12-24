a6f79ab7a15648300615788b0b4b3fb9508ff38f
georgezouq <zousongqi@foxmail.com>
2017-12-24 23:44:02 +0800

    build new model for new data

--- ai/LSTM/LSTM_Stock_Quandl.py
+++ ai/LSTM/LSTM_Stock_Quandl.py
@@ -1,6 +1,11 @@
 import quandl
-import get_stock_data from get_data_quandl
-
+from get_data_quandl import get_stock_data
+from split_data import load_data
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.recurrent import LSTM
+# from keras.models import load_model
+import keras
 # Input of the model
 
 quandl.ApiConfig.api_key = 'zpFWg7jpwtBPmzA8sT2Z'
@@ -15,3 +20,25 @@ stock_name = 'AAPL'
 # Pull the data from Quandl first
 
 df = get_stock_data(stock_name, ma=[50, 100, 200])
+
+X_train, y_train, X_test, y_test = load_data(df, seq_len)
+
+# Model Execution
+
+def build_model(shape, neurons, dropout, decay):
+    model = Sequential()
+
+    model.add(LSTM(neurons[0], input_shape=(shape[0], shape[1]), return_sequences=True))
+    model.add(Dropout(dropout))
+
+    model.add(LSTM(neurons[1], input_shape=(shape[0], shape[1]), return_sequences=False))
+    model.add(Dropout(dropout))
+
+    model.add(Dense(neurons[2],kernel_initializer="uniform",activation='relu'))
+    model.add(Dense(neurons[3],kernel_initializer="uniform",activation='linear'))
+    # model = load_model('my_LSTM_stock_model1000.h5')
+    adam = keras.optimizers.Adam(decay=decay)
+    model.compile(loss='mse',optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
+#
