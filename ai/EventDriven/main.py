0d28a92a4bc5b8396575ba70f01b583220d33c99
georgezouq <zousongqi@foxmail.com>
2018-01-19 17:40:03 +0800

    Add model to main file

--- ai/EventDriven/main.py
+++ ai/EventDriven/main.py
@@ -1,9 +1,26 @@
 from dataHelper import queryNewsFromNYTimes, queryStockPrice, mergingData
+from model import run_model, sentimentIntensity, processWithData, normalize_data, generate_hyperparameters, split_train_and_test_set
 
-# Process with News and Stock Price Data
+# ### Process Data###
 
 # queryNewsFromNYTimes()
-interpolateDf = queryStockPrice()
-mergingData(interpolateDf)
+# interpolateDf = queryStockPrice()
+# mergingData(interpolateDf)
 
-# Build Model
+# ### Build Model ###
+
+print('### Build Model ###')
+df, df_stocks = sentimentIntensity(processWithData())
+print('### read data and sentiment intensity success:', df.head())
+
+datasetNorm = normalize_data(df)
+print('### normalize data success:', datasetNorm.head())
+
+hp = generate_hyperparameters(len(datasetNorm.index))
+xTrain, yTrain, xTest, yTest = split_train_and_test_set(datasetNorm, hp)
+print('### split train and test data success:', xTrain.head(), yTrain.head(), xTest.head(), yTest.head())
+
+# ### Run model ###
+
+run_model(
+    {xTrain, yTrain, xTest, yTest}, hp)
